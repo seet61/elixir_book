f7dad9344ddde7dca6cfd52ad46763c36206d493
dmitry.a.arefyev <dmitry.a.arefyev@tele2.ru>
2024-03-13 10:30:18 +0300

    feat: use Supervisor

--- lib/chapter8/supervised_todo_cache/lib/todo/system.ex
+++ lib/chapter8/supervised_todo_cache/lib/todo/system.ex
@@ -2,9 +2,14 @@ defmodule Todo.System do
   @moduledoc """
   8.3.4. Обертка супервизора
   """
+  use Supervisor
 
   def start_link do
-    Supervisor.start_link(
+    Supervisor.start_link(__MODULE__, nil)
+  end
+
+  def init(_) do
+    Supervisor.init(
       [Todo.Cache],
       strategy: :one_for_one
     )
